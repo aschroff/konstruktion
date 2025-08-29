e004a08c9a60cd5571902dc895e7b80cd40a0730
andreasschroff <andreas@schroff-online.com>
2025-08-29 20:45:09 +0200

    isort

--- src/app/domains/auftrag/crud.py
+++ src/app/domains/auftrag/crud.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
+
 from .models import Auftrag
 from .schemas import AuftragCreate
+
 
 def create_auftrag(db: Session, auftrag: AuftragCreate, result: float) -> Auftrag:
     db_obj = Auftrag(name=auftrag.name, result=result)
@@ -9,5 +11,6 @@ def create_auftrag(db: Session, auftrag: AuftragCreate, result: float) -> Auftra
     db.refresh(db_obj)
     return db_obj
 
-def get_auftrag_by_id(db: Session, auftrag_id: int)-> Auftrag | None:
+
+def get_auftrag_by_id(db: Session, auftrag_id: int) -> Auftrag | None:
     return db.query(Auftrag).filter(Auftrag.id == auftrag_id).first()
